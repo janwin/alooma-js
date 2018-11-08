b236c2b390269bd1571d71b0b3f192139fc6da4f
Ram Amar <amar.rami@gmail.com>
2018-11-08 11:41:09 +0200

    update bundling examples

--- examples/amd-requirejs/index.js
+++ examples/amd-requirejs/index.js
@@ -1,6 +1,7 @@
 requirejs(['./alooma.amd'], function(alooma) {
 
     alooma.init("FAKE_TOKEN", {
+        api_host: "",
         debug: true,
         loaded: function() {
             alooma.track('loaded() callback works but is unnecessary');
--- examples/commonjs-browserify/index.js
+++ examples/commonjs-browserify/index.js
@@ -1,6 +1,7 @@
 var alooma = require('../../build/alooma.cjs.js');
 
 alooma.init("FAKE_TOKEN", {
+    api_host: "",
     debug: true,
     loaded: function() {
         alooma.track('loaded() callback works but is unnecessary');
